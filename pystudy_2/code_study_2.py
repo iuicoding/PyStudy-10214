a180f7fa425d3b6bb150cd7752fb685a223f9bdc
Namulove <jinubaek@naver.com>
2022-06-22 15:03:11 +0900

    update

---
+++ pystudy_2/code_study_2.py
@@ -0,0 +1,47 @@
+a=float(input('a'))
+b=float(input('b'))
+c=float(input('c'))
+
+print('michael jackson', a,b,c)
+print('michael jordan')
+
+score = int(input('score'))
+
+
+if score == 2:
+    print(score*a)
+elif score <=2:
+    print(score*b)
+elif score >= 2:
+    print('horray')
+else:
+    print('what!')
+if a >= score:
+    print('sdfsdfsdfsdfsdfsdf')
+elif a <= score:
+    print('123')
+else:
+    a = b
+    print(b)
+
+gogangmin = 1
+puyopuyo = int(input('puyopuyo combo'))
+if puyopuyo >= 10:
+    print('gogangmin dead by daylight number',puyopuyo-gogangmin)
+elif puyopuyo == 10:
+    print('gogangmin die on perfect',puyopuyo)
+else:
+    print('goganmin overpowered puyupuyo by', puyopuyo*puyopuyo)
+t=float(input('t'))
+bb=float(input('bb'))
+cc=float(input('cc'))
+s=(t+bb+cc)/2
+A=(s*(s-t)*(s-bb)*(s-cc))**(1/2)
+print(A)
+
+MS=37
+SS=22
+LS=31
+steroid=21
+
+print(MS*steroid+SS*steroid+LS*steroid,"you can't stop michael jordan")
